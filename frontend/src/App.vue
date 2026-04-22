3d64b7fc5777b7be441027631744636d3e267332
tancau <tancau@example.com>
2026-04-22 18:16:44 +0800

    fix: use ShushanGame class in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,48 +1,48 @@
 <template>
   <div id="app">
-    <h1>蜀山剑侠传</h1>
     <div id="game-container"></div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { MenuScene } from './scenes/MenuScene'
-import { MapScene } from './scenes/MapScene'
-import { BattleScene } from './scenes/BattleScene'
-import Phaser from 'phaser'
+import { ShushanGame } from './game/Game'
 
 onMounted(() => {
-  const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    parent: 'game-container',
-    scene: [MenuScene, MapScene, BattleScene],
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 0 },
-        debug: false
-      }
-    }
-  }
-
-  new Phaser.Game(config)
+  // 使用完整的游戏系统
+  new ShushanGame('game-container')
 })
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #0F1419;
 }
 
 #game-container {
+  width: 100%;
+  height: 100%;
+}
+
+#game-container canvas {
+  display: block;
   margin: 0 auto;
 }
 </style>
